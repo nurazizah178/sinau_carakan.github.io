a3a2b527bfe1f0763b7ba21f0e002bea4af642ed
Nur Azizah <80570121+nurazizah178@users.noreply.github.com>
2023-05-03 12:47:47 +0700

    Add files via upload

---
+++ aksara.js
@@ -0,0 +1,124 @@
+function bunyiha() {
+    var ha = new Audio('ejaan/ha1.m4a');
+    ha.play();
+}
+
+function bunyina() {
+    var bel = new Audio('ejaan/na1.m4a');
+    bel.play();
+}
+
+function bunyica() {
+    var bel = new Audio('ejaan/ca1.m4a');
+    bel.play();
+}
+
+function bunyira() {
+    var bel = new Audio('ejaan/ra1.m4a');
+    bel.play();
+}
+
+function bunyika() {
+    var bel = new Audio('ejaan/ka1.m4a');
+    bel.play();
+}
+
+function bunyida() {
+    var bel = new Audio('ejaan/da1.m4a');
+    bel.play();
+}
+
+function bunyita() {
+    var bel = new Audio('ejaan/ta1.m4a');
+    bel.play();
+}
+
+function bunyisa() {
+    var bel = new Audio('ejaan/sa1.m4a');
+    bel.play();
+}
+
+function bunyiwa() {
+    var bel = new Audio('ejaan/wa1.m4a');
+    bel.play();
+}
+
+function bunyila() {
+    var bel = new Audio('ejaan/la1.m4a');
+    bel.play();
+}
+
+function bunyipa() {
+    var bel = new Audio('ejaan/pa.m4a');
+    bel.play();
+}
+
+function bunyidha() {
+    var bel = new Audio('ejaan/dha.m4a');
+    bel.play();
+}
+
+function bunyija() {
+    var bel = new Audio('ejaan/ja.m4a');
+    bel.play();
+}
+
+function bunyiya() {
+    var bel = new Audio('ejaan/ya.m4a');
+    bel.play();
+}
+
+function bunyinya() {
+    var bel = new Audio('ejaan/nya.m4a');
+    bel.play();
+}
+
+function bunyipa() {
+    var bel = new Audio('ejaan/pa.m4a');
+    bel.play();
+}
+
+function bunyidha() {
+    var bel = new Audio('ejaan/dha.m4a');
+    bel.play();
+}
+
+function bunyija() {
+    var bel = new Audio('ejaan/ja.m4a');
+    bel.play();
+}
+
+function bunyiya() {
+    var bel = new Audio('ejaan/ya.m4a');
+    bel.play();
+}
+
+function bunyinya() {
+    var bel = new Audio('ejaan/nya.m4a');
+    bel.play();
+}
+
+function bunyima() {
+    var bel = new Audio('ejaan/ma.m4a');
+    bel.play();
+}
+
+function bunyiga() {
+    var bel = new Audio('ejaan/ga.m4a');
+    bel.play();
+}
+
+function bunyiba() {
+    var bel = new Audio('ejaan/ba.m4a');
+    bel.play();
+}
+
+function bunyitha() {
+    var bel = new Audio('ejaan/tha.m4a');
+    bel.play();
+}
+
+function bunyinga() {
+    var bel = new Audio('ejaan/nga.m4a');
+    bel.play();
+}
